7656c1f7c62a30112b848adc7b6b057f8c85970a
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2024-06-18 14:42:37 +0200

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,9 +1,7 @@
-# Runtime: 498 ms
-# Memory: 17.25 MB
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
-        print (nums)
         for i in range(length):
             diff = target-nums[i]
             if diff in nums:
